a5f72e82a7f7b3fff009ba04998a13f0bcf580a4
Nitai Hadad <nitai.hadad@forter.com>
2024-11-10 16:10:00 +0200

    push

--- t5_finetune_model/app.py
+++ t5_finetune_model/app.py
@@ -46,7 +46,7 @@ def main():
     st.title("YouTube Video Title Generator")
     st.header(os.listdir())
     st.header(os.getcwd())
-    os.chdir("title_yt_videos")
+    os.chdir("t5_finetune_model")
 
 
 
